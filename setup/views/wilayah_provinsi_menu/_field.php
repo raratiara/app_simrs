af84d5e9521423dfa443470cf5f8f7cc0df691fe
raratiara <39117751+raratiara@users.noreply.github.com>
2025-02-07 07:28:00 +0700

    Delete setup directory

--- setup/views/wilayah_provinsi_menu/_field.php
+++
@@ -1,7 +0,0 @@
-									<div class="form-group">
-										<label class="col-md-3 control-label no-padding-right">Nama Provinsi <span class="required">*</span></label>
-										<div class="col-md-9">
-											<?=$txtprovinsi;?>
-										</div>
-									</div>
-									
