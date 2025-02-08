abf9db7d6bbb3c3733e33675117f7fc64c6c2dc3
raratiara <39117751+raratiara@users.noreply.github.com>
2025-02-08 08:48:22 +0700

    Add files via upload

---
+++ setup/views/dokter_menu/_field.php
@@ -0,0 +1,41 @@
+									
+		
+
+
+									<div class="form-group">
+										<label class="col-md-3 control-label no-padding-right">Poli <span class="required">*</span></label>
+										<div class="col-md-9">
+											<?=$selpoli;?>
+											<input type="hidden" id="id" name="id">
+										</div>
+									</div>
+									<div class="form-group">
+										<label class="col-md-3 control-label no-padding-right">Spesialis <span class="required">*</span></label>
+										<div class="col-md-9">
+											<?=$txtspesialis;?>
+										</div>
+									</div>
+									<div class="form-group">
+										<label class="col-md-3 control-label no-padding-right">Nama <span class="required">*</span></label>
+										<div class="col-md-9">
+											<?=$txtnama;?>
+										</div>
+									</div>
+									<div class="form-group">
+										<label class="col-md-3 control-label no-padding-right">Foto <span class="required">*</span></label>
+										<div class="col-md-9">
+											<?=$txtfoto;?>
+										</div>
+									</div>
+									<div class="form-group">
+										<label class="col-md-3 control-label no-padding-right">Tgl Join </label>
+										<div class="col-md-9">
+											<input type="date" id="tgl_join" name="tgl_join">
+										</div>
+									</div>
+
+
+
+									
+
+									
