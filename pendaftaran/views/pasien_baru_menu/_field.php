b58b785a23aac7d4dc6f20fe4b1f970e99c69b63
raratiara <39117751+raratiara@users.noreply.github.com>
2025-02-12 08:09:05 +0700

    Add files via upload

---
+++ pendaftaran/views/pasien_baru_menu/_field.php
@@ -0,0 +1,202 @@
+									<div class="row">
+										<div class="col-md-12 col-sm-12">
+											<div class="form-group">
+												<label class="col-md-2 control-label no-padding-right">Nomor RM </label>
+												<div class="col-md-4">
+													<?=$txtnorm;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-2 control-label no-padding-right">Nama Pasien </label>
+												<div class="col-md-4">
+													<?=$txtnamapasien;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-2 control-label no-padding-right">Tempat/Tgl Lahir </label>
+												<div class="col-md-4">
+													<?=$txtttl;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-2 control-label no-padding-right">Jenis Kelamin </label>
+												<div class="col-md-4">
+													<?=$rdojeniskelamin;?>
+												</div>
+											</div>
+										</div>
+										<div class="col-md-6 col-sm-12">
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Agama </label>
+												<div class="col-md-8">
+													<?=$selagama;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Pekerjaan</label>
+												<div class="col-md-8">
+													<?=$txtpekerjaan;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">No HP</label>
+												<div class="col-md-8">
+													<?=$txtnohp;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Alamat Tinggal</label>
+												<div class="col-md-8">
+													<?=$txtalamattinggal;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Prov</label>
+												<div class="col-md-8">
+													<?=$selprov_alamattinggal;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Kecamatan</label>
+												<div class="col-md-8">
+													<?=$selkec_alamattinggal;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">No BPJS</label>
+												<div class="col-md-8">
+													<?=$txtnobpjs;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">No Identitas</label>
+												<div class="col-md-8">
+													<?=$txtnoidentitas;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Alamat Identitas</label>
+												<div class="col-md-8">
+													<?=$txtalamatiden;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Prov</label>
+												<div class="col-md-8">
+													<?=$selprov_alamatiden;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Kecamatan</label>
+												<div class="col-md-8">
+													<?=$selkec_alamatiden;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Nama Lengkap Ayah Pasien</label>
+												<div class="col-md-8">
+													<?=$txtnamaayah;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Nama Lengkap Suami/Istri</label>
+												<div class="col-md-8">
+													<?=$txtnamapasangan;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Nama Penanggung Jawab Pasien</label>
+												<div class="col-md-8">
+													<?=$txtnamapenanggung;?>
+												</div>
+											</div>
+										</div>
+										<div class="col-md-6 col-sm-12">
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Pendidikan </label>
+												<div class="col-md-8">
+													<?=$selpendidikan;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Status Kawin </label>
+												<div class="col-md-8">
+													<?=$selstatuskawin;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Email </label>
+												<div class="col-md-8">
+													<?=$txtemail;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Alamat Tinggal </label>
+												<div class="col-md-8">
+													<?=$txtdept;?><?=$txtdepartemen;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Kab/Kota </label>
+												<div class="col-md-8">
+													<?=$selkota_alamattinggal;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Kelurahan </label>
+												<div class="col-md-8">
+													<?=$selkel_alamattinggal;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">No BPJS </label>
+												<div class="col-md-8">
+													<?=$txtnobpjs;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">No Identitas </label>
+												<div class="col-md-8">
+													<?=$txtnoidentitas;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Alamat Identitas </label>
+												<div class="col-md-8">
+													<?=$txtalamatiden;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Kab/Kota </label>
+												<div class="col-md-8">
+													<?=$selkota_alamatiden;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Kelurahan </label>
+												<div class="col-md-8">
+													<?=$selkel_alamatiden;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">Nama Lengkap Ibu Pasien </label>
+												<div class="col-md-8">
+													<?=$txtnamaibu;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">No HP </label>
+												<div class="col-md-8">
+													<?=$txtnohppasangan;?>
+												</div>
+											</div>
+											<div class="form-group">
+												<label class="col-md-4 control-label no-padding-right">No HP </label>
+												<div class="col-md-8">
+													<?=$txtnohppenanggung;?>
+												</div>
+											</div>
+										</div>
+									</div>
+									
+									
